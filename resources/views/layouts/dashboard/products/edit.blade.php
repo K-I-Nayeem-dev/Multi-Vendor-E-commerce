eb153b3d5341975154d18aed8cf9a82c7daf8272
Kamrul Islam Nayeem <naeem8541@gmail.com>
2023-11-09 16:24:20 +0600

    add select option in product edit section

--- resources/views/layouts/dashboard/products/edit.blade.php
+++ resources/views/layouts/dashboard/products/edit.blade.php
@@ -60,7 +60,7 @@
                             <label for="exampleInputEmail1" class="form-label">Select Category</label>
                             <select class="form-control" name="category_id">
                                 @foreach ($categories as $category)
-                                    <option  value="{{ $category->id }}">{{ $category->category_name }}</option>
+                                    <option  value="{{ $category->id }}" {{ $category->id == $user->category_id ? 'selected' : '' }}>{{ $category->category_name}} </option>
                                 @endforeach
                             </select>
                         </div>
